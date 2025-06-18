9c86971e21daed1d3376bc9a5e7e48981b89ee14
Goltz <niklas.goltz@adesso.de>
2025-06-18 11:09:41 +0200

    join, leave

---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource, AudioPlayerStatus ,Events } = require('@discordjs/voice');
+const { join } = require('path');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,   
+        GatewayIntentBits.GuildMessages,    
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates   
+    ]
+});
+
+const resource = createAudioResource(join(__dirname, 'music', 'test.mp3'));
+console.log(resource)
+
+const player = createAudioPlayer();
+
+client.once('ready', () => {
+    console.log(`Bot ist online! Eingeloggt als ${client.user.tag}`);
+});
+
+let connection;
+
+player.on('error', error => {
+	console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+	player.play(getNextResource());
+});
+
+player.on(AudioPlayerStatus.Playing, () => {
+    console.log('The audio player has started playing!');
+});
+
+player.on(AudioPlayerStatus.Idle, () => {
+    console.log('The audio player is now idle!');
+});
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    switch (message.content) {
+        case '!join':   if (!message.member.voice.channel) {
+                            return message.reply('Du musst in einem Sprachkanal sein, um diesen Befehl zu verwenden!'); 
+                        } else {
+                            connection = joinVoiceChannel({
+                                channelId: message.member.voice.channel.id,
+                                guildId: message.guild.id, 
+                                adapterCreator: message.guild.voiceAdapterCreator, 
+                            });
+                            connection.subscribe(player);
+                            player.play(resource);
+                        }
+                        break;
+        case '!leave':  connection = getVoiceConnection(message.guild.id);
+                        if (connection) {
+                            connection.destroy();
+                            player.stop();
+                        } else {
+                            message.reply('Ich bin in keinem Sprachkanal!');
+                        }
+                        break;
+        }
+});
+
+
+
+client.login(process.env.TOKEN);
